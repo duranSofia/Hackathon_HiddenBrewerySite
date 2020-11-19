7bfca47414bbab95f627ac22edd45892c6ca913d
Sofia Duran <durannsofia@gmail.com>
2020-11-19 17:01:28 +0100

    added basic style for homepage

--- hiddenbrewery/src/Pages/Home.js
+++ hiddenbrewery/src/Pages/Home.js
@@ -1,135 +1,47 @@
-// import React, { Component } from "react";
-// // import "./home.css";
-// import { makeStyles } from "@material-ui/core/styles";
-// import clsx from "clsx";
-// import Card from "@material-ui/core/Card";
-// import CardHeader from "@material-ui/core/CardHeader";
-// import CardMedia from "@material-ui/core/CardMedia";
-// import CardContent from "@material-ui/core/CardContent";
-// import CardActions from "@material-ui/core/CardActions";
-// import Collapse from "@material-ui/core/Collapse";
-// import Avatar from "@material-ui/core/Avatar";
-// import IconButton from "@material-ui/core/IconButton";
-// import Typography from "@material-ui/core/Typography";
-// import { red } from "@material-ui/core/colors";
-// import FavoriteIcon from "@material-ui/icons/Favorite";
-// import ShareIcon from "@material-ui/icons/Share";
-// import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
-// import MoreVertIcon from "@material-ui/icons/MoreVert";
+import React, { Component } from "react";
+import "./home.css";
 
-// const useStyles = makeStyles((theme) => ({
-//   root: {
-//     maxWidth: 345,
-//   },
-//   media: {
-//     height: 0,
-//     paddingTop: "56.25%", // 16:9
-//   },
-//   expand: {
-//     transform: "rotate(0deg)",
-//     marginLeft: "auto",
-//     transition: theme.transitions.create("transform", {
-//       duration: theme.transitions.duration.shortest,
-//     }),
-//   },
-//   expandOpen: {
-//     transform: "rotate(180deg)",
-//   },
-//   avatar: {
-//     backgroundColor: red[500],
-//   },
-// }));
-
-// export default function Home() {
-//   return (
-//     <div>
-//       <div>Search categories</div>
-//       const classes = useStyles(); const [expanded, setExpanded] =
-//       React.useState(false); const handleExpandClick = () =>{" "}
-//       {setExpanded(!expanded)}
-//       return (
-//       <Card className={classes.root}>
-//         <CardHeader
-//           avatar={
-//             <Avatar aria-label="recipe" className={classes.avatar}>
-//               R
-//             </Avatar>
-//           }
-//           action={
-//             <IconButton aria-label="settings">
-//               <MoreVertIcon />
-//             </IconButton>
-//           }
-//           title="Shrimp and Chorizo Paella"
-//           subheader="September 14, 2016"
-//         />
-//         <CardMedia
-//           className={classes.media}
-//           image="/static/images/cards/paella.jpg"
-//           title="Paella dish"
-//         />
-//         <CardContent>
-//           <Typography variant="body2" color="textSecondary" component="p">
-//             This impressive paella is a perfect party dish and a fun meal to
-//             cook together with your guests. Add 1 cup of frozen peas along with
-//             the mussels, if you like.
-//           </Typography>
-//         </CardContent>
-//         <CardActions disableSpacing>
-//           <IconButton aria-label="add to favorites">
-//             <FavoriteIcon />
-//           </IconButton>
-//           <IconButton aria-label="share">
-//             <ShareIcon />
-//           </IconButton>
-//           <IconButton
-//             className={clsx(classes.expand, {
-//               [classes.expandOpen]: expanded,
-//             })}
-//             onClick={handleExpandClick}
-//             aria-expanded={expanded}
-//             aria-label="show more"
-//           >
-//             <ExpandMoreIcon />
-//           </IconButton>
-//         </CardActions>
-//         <Collapse in={expanded} timeout="auto" unmountOnExit>
-//           <CardContent>
-//             <Typography paragraph>Method:</Typography>
-//             <Typography paragraph>
-//               Heat 1/2 cup of the broth in a pot until simmering, add saffron
-//               and set aside for 10 minutes.
-//             </Typography>
-//             <Typography paragraph>
-//               Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet
-//               over medium-high heat. Add chicken, shrimp and chorizo, and cook,
-//               stirring occasionally until lightly browned, 6 to 8 minutes.
-//               Transfer shrimp to a large plate and set aside, leaving chicken
-//               and chorizo in the pan. Add pimentón, bay leaves, garlic,
-//               tomatoes, onion, salt and pepper, and cook, stirring often until
-//               thickened and fragrant, about 10 minutes. Add saffron broth and
-//               remaining 4 1/2 cups chicken broth; bring to a boil.
-//             </Typography>
-//             <Typography paragraph>
-//               Add rice and stir very gently to distribute. Top with artichokes
-//               and peppers, and cook without stirring, until most of the liquid
-//               is absorbed, 15 to 18 minutes. Reduce heat to medium-low, add
-//               reserved shrimp and mussels, tucking them down into the rice, and
-//               cook again without stirring, until mussels have opened and rice is
-//               just tender, 5 to 7 minutes more. (Discard any mussels that don’t
-//               open.)
-//             </Typography>
-//             <Typography>
-//               Set aside off of the heat to let rest for 10 minutes, and then
-//               serve.
-//             </Typography>
-//           </CardContent>
-//         </Collapse>
-//       </Card>
-//       ); }<div>input 1 </div>
-//       <div>input 2</div>
-//       <div>Map</div>
-//       <div>list of cards</div>
-//     </div>
-//   );
-// }
+export default class Home extends Component {
+  render() {
+    return (
+      <div>
+        <div>
+          <div>Search categories</div>
+          <div className="menu-selector">
+            <div>input 1 </div>
+            <div>input 2 </div>
+          </div>
+        </div>
+        <div className="map">Map</div>
+        <h2>list of cards</h2>
+        <div className="cards-section">
+          <div className="card">
+            <div className="image-card"></div>
+            <div>
+              <h3>Bar Name</h3>
+              <p>
+                Open Brewery DB is a free dataset and API with public
+                information on breweries, cideries, brewpubs, and bottleshops.
+                The goal of Open Brewery DB is to maintain an open-source,
+                community-driven dataset and provide a public API. It is our
+              </p>
+            </div>
+          </div>
+          <br />
+          <div className="card">
+            <div className="image-card"></div>
+            <div>
+              <h3>Bar Name</h3>
+              <p>
+                Open Brewery DB is a free dataset and API with public
+                information on breweries, cideries, brewpubs, and bottleshops.
+                The goal of Open Brewery DB is to maintain an open-source,
+                community-driven dataset and provide a public API. It is our
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
