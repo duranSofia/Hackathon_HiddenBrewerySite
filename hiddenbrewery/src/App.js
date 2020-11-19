acb190427093a09abe65d292c0621dcd3cc8259e
Omar Dieh <omardieh@gmail.com>
2020-11-19 18:08:58 +0100

    fake home page elements added

--- hiddenbrewery/src/App.js
+++ hiddenbrewery/src/App.js
@@ -9,7 +9,7 @@ function App() {
     <Router>
       <Switch>
         <Route path='/' component={FakeHome} exact />
-        <Route path='/redirect' component={Home} />
+        <Route path='/redirect' component={Home} exact />
       </Switch>
     </Router>
   );
---
+++ hiddenbrewery/src/utils/fakeData.js
@@ -0,0 +1,52 @@
+import washingMachine from '../media/fakeMedia/01.jpg';
+import cleanerVacuum from '../media/fakeMedia/02.jpg';
+import multiBlender from '../media/fakeMedia/03.jpg';
+import specialBlender from '../media/fakeMedia/04.jpg';
+import gas from '../media/fakeMedia/05.jpg';
+import fridge from '../media/fakeMedia/06.jpg';
+import oven from '../media/fakeMedia/07.jpg';
+
+export const fakeData = [
+    {
+    id:1, 
+    name: "Washing Machine",
+    title: 'Washing Machine',
+    img:washingMachine
+  }, 
+    {
+    id:2, 
+    name: "Vaccum Cleaner", 
+    title: 'Vaccum Cleaner',
+    img:cleanerVacuum
+  },
+    {
+    id:3, 
+    name: "Multi-functions Blender", 
+    title: 'Multi-functions Blender',
+    img:multiBlender
+  }, 
+    {
+    id:4, 
+    name: "Special Advanced blender", 
+    title: 'Special Advanced blender',
+    img:specialBlender
+  }, 
+    {
+    id:5, 
+    name: "Gas Rango", 
+    title: 'Gas Rango',
+    img:gas
+  }, 
+    {
+    id:6, 
+    name: "Admiral Fridge", 
+    title: 'test test 2',
+    img:fridge
+  }, 
+    {
+    id:7, 
+    name: "Her Oven", 
+    title: 'test test 2',
+    img:oven
+  },  
+]
