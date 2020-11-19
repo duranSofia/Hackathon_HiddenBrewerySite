e4e65911f0453e3946e557804f0040667a7bf683
GuilhermeC18 <52559295+GuilhermeC18@users.noreply.github.com>
2020-11-19 19:40:48 +0100

    map added

--- hiddenbrewery/src/App.js
+++ hiddenbrewery/src/App.js
@@ -1,14 +1,44 @@
 import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import MyMapComponent from "./components/Map";
 
+
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      data: [],
+      loaded: false
+    }
+  }
+
+  componentDidMount(){
+    fetch("https://api.openbrewerydb.org/breweries?by_city=new_york")
+    .then(resp => resp.json())
+    .then((places) => this.setState({data: places, loaded: true}));
+      }
+  render() {
+    console.log("fetch", this.state.data);
+    return (
+       <div>
+         {this.state.loaded && <MyMapComponent isMarkerShown places={this.state.data} />}
+       </div>
+    )
+  }
+}
+
+
+/*
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <p>Hackathon Project</p>
+         <MyMapComponent isMarkerShown  />
       </header>
     </div>
   );
 }
-
+*/
 export default App;
