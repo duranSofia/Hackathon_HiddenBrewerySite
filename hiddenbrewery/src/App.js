9e8083d4993d431223e987574988911b5780e5fa
SebasBar <toolt2y@gmail.com>
2020-11-20 09:04:41 +0100

    component completed

--- hiddenbrewery/src/App.js
+++ hiddenbrewery/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Categories from './components/Categories'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <p>Hackathon Project</p>
+        <Categories />
       </header>
     </div>
   );
